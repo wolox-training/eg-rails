a200a7064722d305ad7b4bcc925c20f49a1fc47d
Enrique GC <enrique.garcia@wolox.cl>
2019-05-29 12:58:13 -0400

    Fix api request for csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 end
