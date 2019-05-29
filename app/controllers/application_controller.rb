bd21b1ca4375ebae68bcd3bfdeb3e6c7a698c285
Enrique GC <enrique.garcia@wolox.cl>
2019-05-29 13:09:52 -0400

    Fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@
 
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
+  protect_from_forgery with: :null_session, only: proc { |c| c.request.format.json? }
 end
