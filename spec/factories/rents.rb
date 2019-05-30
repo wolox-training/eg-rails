ab94c0c23690f436629b7449cfa139849006c2d1
Enrique GC <enrique.garcia@wolox.cl>
2019-05-30 16:10:22 -0400

    Add factories and test for rent

---
+++ spec/factories/rents.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :rent do
+    user
+    book
+    start_at { '2019-05-30 14:16:55' }
+    end_at { '2019-05-30 14:16:55' }
+  end
+end
---
+++ spec/models/rent_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Rent do
+  subject(:rent) { create(:rent) }
+
+  it { is_expected.to be_valid }
+
+  # Associations
+  it { should belong_to(:user) }
+  it { should belong_to(:book) }
+
+  # Validations
+  it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_presence_of(:book) }
+  it { is_expected.to validate_presence_of(:start_at) }
+  it { is_expected.to validate_presence_of(:end_at) }
+end
