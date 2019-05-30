b082dfacd8e124539ba30c7b4009647f1895151d
Enrique GC <enrique.garcia@wolox.cl>
2019-05-30 16:25:50 -0400

    Add user validations, rent association and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   include DeviseTokenAuth::Concerns::User
+
+  has_many :rents
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe User do
+  subject(:user) { build_stubbed(:user) }
+
+  it { is_expected.to be_valid }
+
+  # Associations
+  it { should have_many(:rents) }
+
+  # Validations
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+end
