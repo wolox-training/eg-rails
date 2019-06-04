4a5db1ea59c9856441a980c34949a4e478616250
Enrique GC <enrique.garcia@wolox.cl>
2019-06-04 16:04:05 -0400

    Change app controller name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApiController < ActionController::API
+class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
