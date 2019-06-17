3c2d8e975cb84ce92774e5417a2ede067a8e7091
Enrique GC <enrique.garcia@wolox.cl>
2019-06-17 17:20:41 -0400

    Add rescue for not authorized action

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -5,6 +5,7 @@ module Api
     before_action :authenticate_api_v1_user!
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
     private
 
@@ -13,6 +14,13 @@ module Api
         message: "Not found with id #{params[:id]}",
         code: :not_found
       }, status: :not_found
+    end
+
+    def user_not_authorized
+      render json: {
+        message: 'You cannot perform this action.',
+        code: :unauthorized
+      }, status: :unauthorized
     end
   end
 end
